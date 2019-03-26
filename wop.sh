2acba4a738f84459457c6b3eedaa3c91ece623e5
wangzki03 <35860608+wangzki03@users.noreply.github.com>
2019-03-26 15:48:34 +0800

    Update wop.sh

--- wop.sh
+++ wop.sh
@@ -353,7 +353,7 @@ service openvpn status
 #Setting USW
 apt-get install ufw
 ufw allow ssh
-ufw allow 1194/tcp
+ufw allow 443/tcp
 sed -i 's|DEFAULT_INPUT_POLICY="DROP"|DEFAULT_INPUT_POLICY="ACCEPT"|' /etc/default/ufw
 sed -i 's|DEFAULT_FORWARD_POLICY="DROP"|DEFAULT_FORWARD_POLICY="ACCEPT"|' /etc/default/ufw
 cat > /etc/ufw/before.rules <<-END
